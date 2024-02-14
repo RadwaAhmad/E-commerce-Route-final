5a3ff07f53c6e077a52e1e7307d9cb17be872373
Radwa Awad <radwaawad@Radwas-MacBook-Air.local>
2024-02-14 19:01:44 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Layout from "./components/Layout/Layout"
+import Home from "./components/Home/Home"
+import Brands from "./components/Brands/Brands"
+import Carts from "./components/Carts/Carts"
+import Footer from "./components/Footer/Footer"
+import Login from "./components/Login/Login"
+import Navbar from "./components/Navbar/Navbar"
+import Notfound from "./components/Notfound/Notfound"
+import Products from "./components/Products/Products"
+import Register from "./components/Register/Register"
+
+
+const routers = createBrowserRouter([
+  {
+    path: '/', element: <Layout />, children: [
+      { path: 'Navbar', element: <Navbar /> },
+      { path: 'Footer', element: <Footer /> },
+      { path: 'Login', element: <Login /> },
+      { path: 'Products', element: <Products /> },
+      { path: 'Brands', element: <Brands /> },
+      { path: 'Carts', element: <Carts /> },
+      { path: 'Home', element: <Home /> },
+      { path: 'Register', element: <Register /> },
+      { path: '*', element: <Notfound /> },
+    ]
+  }
+])
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+  return <RouterProvider> router={routers} 
+   </RouterProvider>
+
+
+
+
+/*
+  return <>
+    <Home />
+  </>
+
+ */
+
 }
 
 export default App;
